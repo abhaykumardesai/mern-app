938ccb7cabbeb4fe75839899f12d0f6e17e1339f
Abhay Kumar <abhay385abhay@gmail.com>
2025-03-11 23:09:19 +0530

    Added some UI improvements to Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import { Container, Flex, Text, HStack, Button } from "@chakra-ui/react";
+import { PlusSquareIcon } from "@chakra-ui/icons"; // Import icon
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      NavBar
-    </div>
-  )
-}
+    <Container maxH={"1140px"} px={4}>
+      <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+          base: "column",
+          sm: "row",
+        }}
+      >
+        <Text
+          fontSize={{ base: "22px", sm: "28px" }}
+          fontWeight={"bold"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          bgGradient="linear(to-r, cyan.400, blue.500)"
+          bgClip={"text"}
+        >
+          <Link to={"/"}>Product Store 🛒</Link>
+        </Text>
+
+        {/* Button to Create Page */}
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <PlusSquareIcon fontSize = {20} />
+            </Button>
+          </Link>
+        </HStack>
+      </Flex>
+    </Container>
+  );
+};
 
 export default Navbar
